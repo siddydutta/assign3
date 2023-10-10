5574b196b99a1476dc75c5968812d94587faddda
Siddhartha Dutta <siddharthadutta@protonmail.com>
2023-10-10 11:53:44 +0530

    Assignment 3

--- fingerprint.js
+++ fingerprint.js
@@ -2,7 +2,31 @@
 /* global hash */
 
 // eslint-disable-next-line no-unused-vars
-async function fingerprint () {
-  // TODO: fill this in
-  return hash('userid')
+
+// Function to fetch HTTP headers from the server
+async function fetchHeaders() {
+  try {
+    const response = await fetch('/headers')
+    if (response.ok) {
+      const headers = await response.json()
+      const secHeaders = Object.keys(headers).filter(header => header.startsWith('sec-'))
+        .reduce((acc, header) => {
+          acc[header] = headers[header];
+          return acc;
+        }, {});
+      return secHeaders
+    }
+  } catch (error) {
+    console.error('Error fetching headers:', error)
+  }
+  return null
+}
+
+
+async function fingerprint() {
+  const screenResolution = window.screen.width + 'x' + window.screen.height
+  const language = navigator.language
+  const timezoneOffset = new Date().getTimezoneOffset()
+  const headers = await fetchHeaders()
+  return hash(screenResolution, language, timezoneOffset, headers)
 }
--- your-server-code.js
+++ your-server-code.js
@@ -5,8 +5,9 @@
 const express = require('express')
 const router = express.Router()
 
-// TODO: Add server endpoints here as needed for your fingerprinting technique! (optional)
-// See https://expressjs.com/en/4x/api.html#router for available methods on `router`.
-// e.g.: router.get('/headers', (req, res) => { ... });
+router.get('/headers', (req, res) => {
+    const headers = req.headers
+    res.json(headers)
+  });
 
 module.exports = router
